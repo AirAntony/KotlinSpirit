a9ac3a570143401c99737ec78e71e0fcf97ae227
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-17 00:24:35 +0300

    KS-2-3-Gagarin. Task is done

---
+++ src/main/kotlin/Lessons_2/Lessons2_tas3.kt
@@ -0,0 +1,14 @@
+package Lessons_2
+
+fun main(){
+val year = 1961
+    var hour = "09"
+    var minute: String = "07"
+println(year)
+    println(hour)
+    println(minute)
+hour = 10.toString()
+    minute = 55.toString()
+println(hour+":"+minute
+)
+}
