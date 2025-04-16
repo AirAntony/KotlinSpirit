5ef8b5d96e6c07b7197bf9b20234444959374c12
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-16 23:32:21 +0300

    KS-2-1-types. First task is done

---
+++ src/main/kotlin/Lessons_2/Lessons2_task1.kt
@@ -0,0 +1,14 @@
+package Lessons_2
+
+fun main(){
+
+    var ordersByECom: Int = 75
+    var afterPurchase: String = "Спасибо за покупку"
+
+ //   println(afterPurchase)
+
+    var rectangleHeight: Long = 55L
+    var rectangleLength: Double = 76.3
+
+
+}
