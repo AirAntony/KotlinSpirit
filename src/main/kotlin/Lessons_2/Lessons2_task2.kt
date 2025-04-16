894e35aaa797e16dd49cd84d62df9c4decfbf81b
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-16 23:51:15 +0300

    KS-2-2-print. Secondary task is done

---
+++ src/main/kotlin/Lessons_2/Lessons2_task2.kt
@@ -0,0 +1,19 @@
+package Lessons_2
+
+fun main(){
+
+    var ordersByECom: Int = 75
+    var afterPurchase: String = "Спасибо за покупку"
+    var rectangleHeight: Long = 55L
+    var rectangleLength: Double = 76.3
+println(ordersByECom)
+    println(afterPurchase)
+    println(rectangleHeight)
+    println(rectangleLength)
+    var numberOfEmloyees = 2000
+   // println(numberOfEmloyees)
+    numberOfEmloyees = 1999
+println(numberOfEmloyees)
+
+
+}
