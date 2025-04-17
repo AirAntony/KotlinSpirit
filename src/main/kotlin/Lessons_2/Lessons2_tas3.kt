ac82ee4928eb36ee8c42082502917afacaa97ee9
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-17 22:50:59 +0300

    ks-1-1 upd.

--- src/main/kotlin/Lessons_2/Lessons2_tas3.kt
+++ src/main/kotlin/Lessons_2/Lessons2_tas3.kt
@@ -1,14 +1,15 @@
 package Lessons_2
 
-fun main(){
-val year = 1961
+fun main() {
+    val year: String = "1961"
     var hour = "09"
     var minute: String = "07"
-println(year)
+
+    println(year)
     println(hour)
     println(minute)
-hour = 10.toString()
-    minute = 55.toString()
-println(hour+":"+minute
-)
+    hour = "10"
+    minute = "55"
+
+    print("$hour:$minute")
 }
