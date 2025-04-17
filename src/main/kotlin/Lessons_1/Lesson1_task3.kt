724b9c59473a88df3d0a617da4b139d2c9cc9b3d
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-17 23:01:38 +0300

    ks-1-3 upd.

--- src/main/kotlin/Lessons_1/Lesson1_task3.kt
+++ src/main/kotlin/Lessons_1/Lesson1_task3.kt
@@ -4,3 +4,18 @@ fun main() {
     println("Тествое решение к уроку 1 задача 3")
 
 }
+
+fun main() {
+    val year: String = "1961"
+    var hour = "09"
+    var minute: String = "07"
+
+    println(year)
+    println(hour)
+    println(minute)
+
+    hour = "10"
+    minute = "55"
+
+    print("$hour:$minute")
+}
