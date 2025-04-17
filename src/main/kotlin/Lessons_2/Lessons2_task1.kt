2b5dcde08f6eba5f2379912ffbbfe1094be57180
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-17 22:28:12 +0300

    ks-1-1 upd.

---
+++ src/main/kotlin/Lessons_2/Lessons2_task1.kt
@@ -0,0 +1,10 @@
+package Lessons_2
+
+fun main() {
+
+    val ordersByECom: Int = 75
+    val afterPurchase: String = "Спасибо за покупку"
+
+    val rectangleHeight: Long = 55L
+    val rectangleLength: Double = 76.3
+}
