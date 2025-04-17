4a2d65bf042fce5a7f86930efa0aa5ea4ab6a9b7
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-17 22:34:16 +0300

    ks-1-1 upd.

--- src/main/kotlin/Lessons_2/Lessons2_task2.kt
+++ src/main/kotlin/Lessons_2/Lessons2_task2.kt
@@ -1,19 +1,13 @@
 package Lessons_2
 
-fun main(){
-
-    var ordersByECom: Int = 75
-    var afterPurchase: String = "Спасибо за покупку"
-    var rectangleHeight: Long = 55L
-    var rectangleLength: Double = 76.3
-println(ordersByECom)
-    println(afterPurchase)
-    println(rectangleHeight)
-    println(rectangleLength)
+fun main() {
+    val ordersByECom: Int = 75
+    val afterPurchase: String = "Спасибо за покупку"
     var numberOfEmloyees = 2000
-   // println(numberOfEmloyees)
+
+    // println(numberOfEmloyees)
+
     numberOfEmloyees = 1999
-println(numberOfEmloyees)
 
-
+    println(numberOfEmloyees)
 }
