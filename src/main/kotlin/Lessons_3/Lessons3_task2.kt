234b43690cae50ed8d619e3f87e8b8112928fd40
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-20 22:53:00 +0300

    done

---
+++ src/main/kotlin/Lessons_3/Lessons3_task2.kt
@@ -0,0 +1,11 @@
+package Lessons_3
+
+fun main() {
+    val fotPermanentEmployee = 50*30000
+    val fotAllEmployee = 30*20000+fotPermanentEmployee
+    val averageFOT = fotAllEmployee/80
+
+    println(fotPermanentEmployee)
+    println(fotAllEmployee)
+    println(averageFOT)
+}
