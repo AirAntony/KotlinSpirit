093dd901b1a7a9684f3bb271b45465394490967e
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-21 23:18:15 +0300

    KS-2-2

--- src/main/kotlin/Lessons_3/Lessons3_task2.kt
+++ src/main/kotlin/Lessons_3/Lessons3_task2.kt
@@ -1,9 +1,13 @@
 package Lessons_3
 
 fun main() {
-    val fotPermanentEmployee = 50 * 30000
-    val fotAllEmployee = 30 * 20000 + fotPermanentEmployee
-    val averageFOT = fotAllEmployee / 80
+    val numberOfPermanentEmployee = 50
+    val salaryOfPermanentEmployee = 30000
+    val fotPermanentEmployee = numberOfPermanentEmployee * salaryOfPermanentEmployee
+    val numberOfTrainee = 30
+    val salaryOfTrainee = 20000
+    val fotAllEmployee = numberOfTrainee * salaryOfTrainee + fotPermanentEmployee
+    val averageFOT = fotAllEmployee / (numberOfTrainee + numberOfPermanentEmployee)
 
     println(fotPermanentEmployee)
     println(fotAllEmployee)
