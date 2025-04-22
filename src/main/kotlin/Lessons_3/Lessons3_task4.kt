597978c52b519fb25371e88e3e291732153dd194
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-22 21:23:14 +0300

    KS-2-4

--- src/main/kotlin/Lessons_3/Lessons3_task4.kt
+++ src/main/kotlin/Lessons_3/Lessons3_task4.kt
@@ -3,14 +3,10 @@ package Lessons_3
 fun main() {
     val crystallineOre = 7
     val ironOre = 11
-    val buff = 1.2
-    val bonusCrystallineOre = crystallineOre * buff - crystallineOre
-    val bonusIronOre = ironOre * buff - ironOre
-
-
+    val buff = 20
+    val bonusCrystallineOre = buff * crystallineOre / 100
+    val bonusIronOre = buff * ironOre / 100
 
     println(bonusCrystallineOre.toInt())
     println(bonusIronOre.toInt())
-
-
 }
