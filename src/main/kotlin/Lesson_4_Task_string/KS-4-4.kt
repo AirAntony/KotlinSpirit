de916ab13d3885e440987c59b53e6992ca4f77eb
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-23 22:15:56 +0300

    KS-4-4

--- src/main/kotlin/Lesson_4_Task_string/KS-4-4.kt
+++ src/main/kotlin/Lesson_4_Task_string/KS-4-4.kt
@@ -3,12 +3,12 @@ package Lesson_4_Task_string
 fun main() {
 
     var from = "E2"
-    var whereDo = "E4"
+    var where = "E4"
     var strokeNumber = 1
 
-    println("$from -> $whereDo, $strokeNumber")
+    println("$from -> $where, $strokeNumber")
     from = "D2"
-    whereDo = "D3"
+    where = "D3"
     strokeNumber = 2
-    println("$from -> $whereDo, $strokeNumber")
+    println("$from -> $where, $strokeNumber")
 }
