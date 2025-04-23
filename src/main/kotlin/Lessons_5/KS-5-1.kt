3883eb92add33c02373c51077ee61d6c3b0aea10
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-23 23:06:35 +0300

    KS-5-1

--- src/main/kotlin/Lessons_5/KS-5-1.kt
+++ src/main/kotlin/Lessons_5/KS-5-1.kt
@@ -4,8 +4,11 @@ fun main() {
     val countOfReservesToday = 13
     val contOfReservedTomorrow = 9
     val freeTablesToday: Boolean = countOfReservesToday < All_Tables
+
     println("Досупность столиков на сегодня - $freeTablesToday")
+
     val freeTablesTomorrow: Boolean = contOfReservedTomorrow < All_Tables
+
     println("Досупность столиков на завтра - $freeTablesTomorrow")
 }
 
