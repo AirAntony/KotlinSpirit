1d3b829b8f8c33dbe031d4d03ceb566c503930ff
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-23 22:01:57 +0300

    KS-2-3_3

--- src/main/kotlin/Lessons_3/Lessons3_task3.kt
+++ src/main/kotlin/Lessons_3/Lessons3_task3.kt
@@ -2,11 +2,11 @@ package Lessons_3
 
 fun main() {
     val hourOfDeparture = 9
-    val minutesByHour = 60
+    val minutesByHours = 60
     val minuteOfDeparture = 39
     val minuteTransitTime = 457
-    val hoursOfArrivalTime = minuteTransitTime / minutesByHour + hourOfDeparture
-    val minuteOfArrivalTime = minuteTransitTime / minutesByHour - hoursOfArrivalTime + minuteOfDeparture
+    val hoursOfArrivalTime = minuteTransitTime / minutesByHours + hourOfDeparture
+    val minuteOfArrivalTime = minuteTransitTime / minutesByHours - hoursOfArrivalTime + minuteOfDeparture
 
     print("$hoursOfArrivalTime:$minuteOfArrivalTime")
 }
