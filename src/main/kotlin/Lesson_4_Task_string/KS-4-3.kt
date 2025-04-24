3763e8b5f610f6e8af16a9d6cff1da1afa65d61b
AirAntony <125504001+AirAntony@users.noreply.github.com>
2025-04-24 07:03:24 +0300

    KS-4-3 Объеденил в одну строку. Почему после вывода таблицы умножения, если переназначить переменную с 5 на 7, в таблице ничего не меняется?

--- src/main/kotlin/Lesson_4_Task_string/KS-4-3.kt
+++ src/main/kotlin/Lesson_4_Task_string/KS-4-3.kt
@@ -2,15 +2,17 @@ package Lesson_4_Task_string
 
 fun main() {
 
-    var number = 5
+    var number: Int = 5
 
-    println("$number*2 = ${number * 2}")
-    println("$number*3 = ${number * 3}")
-    println("$number*4 = ${number * 4}")
-    println("$number*5 = ${number * 5}")
-    println("$number*6 = ${number * 6}")
-    println("$number*7 = ${number * 7}")
-    println("$number*8 = ${number * 8}")
-    println("$number*9 = ${number * 9}")
-    number = 8
+    println(
+        """${println("$number*2 = ${number * 2}")}
+        | ${println("$number*3 = ${number * 3}")}
+        | ${println("$number*4 = ${number * 4}")}
+        | ${println("$number*5 = ${number * 5}")}
+        | ${println("$number*6 = ${number * 6}")}
+        | ${println("$number*7 = ${number * 7}")}
+        | ${println("$number*8 = ${number * 8}")}
+        | ${println("$number*9 = ${number * 9}")}
+    """.trimMargin()
+    )
 }
